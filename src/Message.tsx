a64e7ccf4e41ab396e3774cd6fb45e586290c820
Edward Ryan <edwardryan@protonmail.com>
2023-06-26 22:38:11 +0100

    react tutorial part 1

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    // JSX: javascript XML
+    return <h1>Welcome Message.tsx !</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,32 @@
+function ListGroup() {
+    let items = [
+        "New York",
+        "Tokyo",
+        "Dublin",
+        "Paris"
+    ]
+
+    const getMessage = () => {
+        return items.length === 0 && <p>No item found.</p>;
+    }
+
+    return (
+        <>
+            <h1>List</h1>
+            {getMessage()}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        className="list-group-item"
+                        key={item}
+                        onClick={() => console.log(item, index)}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
